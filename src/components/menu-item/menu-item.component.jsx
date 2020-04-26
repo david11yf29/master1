28a3af19db0581b21740f36b121a022408f659ce
david11yf29 <david11yf29@gmail.com>
2020-04-27 00:34:08 +0800

    updated

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = (props) => {
+    return (
+        <div style={{
+            backgroundImage: `url(${props.imageUrl})`
+        }} className={`${props.size} menu-item`}>
+            <div className="content">
+                <h1 className="title">{props.title}</h1>
+                <span className="subtitle">SHOW NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
