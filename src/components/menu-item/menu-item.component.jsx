d7b74bd8cc566bf95de3b50657c28e59df98661e
david11yf29 <david11yf29@gmail.com>
2020-04-27 15:46:55 +0800

    updated

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,11 @@ import './menu-item.styles.scss';
 
 const MenuItem = (props) => {
     return (
-        <div style={{
-            backgroundImage: `url(${props.imageUrl})`
-        }} className={`${props.size} menu-item`}>
+        <div className={`${props.size} menu-item`}>
+            <div className="background-image" style={{
+                backgroundImage: `url(${props.imageUrl})`}}/>
             <div className="content">
-                <h1 className="title">{props.title}</h1>
+                <h1 className="title">{props.title.toUpperCase()}</h1>
                 <span className="subtitle">SHOW NOW</span>
             </div>
         </div>
